8cad937aac0f01b8396810432c799124d5603d35
Amila-siriwardhana <amilasiriwardhana660@gmail.com>
2021-11-12 01:55:51 +0530

    Redirect Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       ecommerce site
+      {/* <RedirectNavBar /> */}
+      <NavBar />
     </div>
   );
 }
