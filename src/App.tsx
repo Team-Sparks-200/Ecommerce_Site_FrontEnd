949c2bfc1f7b8e284cea46e8492976d13a9f51d2
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-11-17 09:58:18 +0530

    footer done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Banner from "./component/Banner";
 import {Container} from "react-bootstrap";
+import Banner from "./component/Banner";
+import Footer from "./component/footer/Footer";
 
 function App() {
   return (
     <Container fluid={true} className='px-0 '>
       <Banner/>
+      <Footer/>
     </Container>
   );
 }
