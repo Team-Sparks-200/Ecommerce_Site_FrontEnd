c58fdade774f8823acc5449f322ad8b8107fb846
Chamindu-Kasun <66759078+Chamindu-Kasun@users.noreply.github.com>
2021-11-19 19:46:34 +0530

    Top navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {Container} from "react-bootstrap";
 import Banner from "./component/Banner";
 import Footer from "./component/footer/Footer";
+import RedirectNavBar from "./component/RedirectNavBar";
+import TopNavBar from "./component/TopNavBar";
 
 function App() {
   return (
     <Container fluid={true} className='px-0 '>
+      <TopNavBar/>
+      <RedirectNavBar/>
       <Banner/>
       <Footer/>
     </Container>
