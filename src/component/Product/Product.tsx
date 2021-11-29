b5c171e8b8c295c8e87bfb08eb12b8bc14ba40dd
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-11-29 10:30:46 +0530

    Categorie UX Done

--- src/component/Product/Product.tsx
+++ src/component/Product/Product.tsx
@@ -1,44 +1,47 @@
 import React from "react";
-import {Row, Col, Card, CardImg, Button} from "react-bootstrap";
-import ConfirmButton from "../common/ConfirmButton";
+import {Row, Col, Button} from "react-bootstrap";
 import NumericInput from 'react-numeric-input';
 
+
 type ProductProps = {
-    item: {
-        id: number
-        name: string,
-        new_price: string,
-        old_price: string
-        image: string
-    }
+  item: {
+    id: number
+    name: string,
+    new_price: string,
+    old_price: string
+    image: string
+  }
 }
 
 const Product: React.FC<ProductProps> = (props: ProductProps) => {
-    const {item} = props;
-    return (
-        <Col lg={3} md={4} xs={6}>
-            <div className="product-bg text-center d-block mb-5">
-                <img src={item.image} alt={item.name} />
-                <h4>{item.name}</h4>
-                <Row className="d-flex flex-wrap mx-lg-2 mx-2 mx-md-0 my-2 mb-lg-3 mb-md-3">
-                    <Col>
-                        <label className="product-price-discount">{item.old_price}</label>
-                    </Col>
-                    <Col>
-                        <label className="product-price">Rs. {item.new_price}</label>
-                    </Col>
-                </Row>
-                <Row className="d-flex flex-wrap mx-lg-2 mx-2 mx-md-4 my-2 mb-lg-3 mb-md-3">
-                    <Col>
-                        <NumericInput className="form-control"/>
-                    </Col>
-                    <Col className="mt-md-2 mt-lg-0 mt-2">
-                        <Button className="btn add-to-cart-button">Add To Cart</Button>
-                    </Col>
-                </Row>
-            </div>
-        </Col>
-    );
+  const {item} = props;
+  return (
+      <Col lg={3} md={4} xs={6}>
+        <div className="product-bg text-center d-block mb-5">
+          <img src={item.image} alt={item.name}/>
+          <h4>{item.name}</h4>
+          <Row className="d-flex flex-wrap mx-lg-2 mx-2 mx-md-0 my-2 mb-lg-3 mb-md-3">
+            <Col>
+              <label className="product-price-discount">{item.old_price}</label>
+            </Col>
+            <Col>
+              <label className="product-price">Rs. {item.new_price}</label>
+            </Col>
+          </Row>
+          <Row className="d-flex flex-wrap mx-lg-2 mx-2 mx-md-4 my-2 mb-lg-3 mb-md-3">
+            <Col>
+              <NumericInput className="form-control" value={0} min={0} style={{
+                btn:{border:"none",background:"transparent",boxShadow:"none",padding: "4px",margin:"2px",height:"15px", fontSize:"16px"},
+                btnUp:{position:"absolute",top:"50%"},
+                btnDown:{position:"absolute",top:0}}}/>
+            </Col>
+            <Col className="mt-md-2 mt-lg-0 mt-2">
+              <Button className="btn add-to-cart-button">Add To Cart</Button>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  );
 }
 
 export default Product;
--- src/component/ProductList.tsx
+++ src/component/ProductList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import Product from "./Product/Product";
 import CoconutImage from '../assets/images/Coconut.jpg';
 import CarrotImage from '../assets/images/Carrot.jpg';
